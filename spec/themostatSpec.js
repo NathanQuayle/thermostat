7af2413b402aeda797b4c5a78877a125f3c36963
Robbie Campbell <roberto251983@gmail.com>
2018-10-03 14:20:36 +0100

    added second test increase temp by 1

--- spec/themostatSpec.js
+++ spec/themostatSpec.js
@@ -2,8 +2,18 @@ describe('Thermostat', function() {
     beforeEach(function() {
         thermostat = new Thermostat();
     });
-    
+
     it('starts with a default temp of 20', function() {
-        expect(thermostat.temp).toBe(20);
+        expect(thermostat.temp).toEqual(20);
     });
+
+    describe('increase', function(){
+        it('increases the thermostat temperature', function(){
+            thermostat.increase()
+            expect(thermostat.temp).toEqual(21)
+        });
+
+    })
+
+
 });
